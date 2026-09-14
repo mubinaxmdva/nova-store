edb20ddcbab2f9d35c7d166bd850f34c2bc9bbc8
Mubina Xamidova <sayfillohh815@gmail.com>
2026-09-14 15:01:45 +0500

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,7 +47,7 @@ function Header({ cartCount = 0 }) {
         >
           NovaStore
         </NavLink>
-<p>{window.innerWidth}</p>
+      <p>{window.innerWidth}</p>
         {/* Desktop navigation */}
         <nav className="nav" aria-label="Main navigation">
           <NavLink to="/">Home</NavLink>
